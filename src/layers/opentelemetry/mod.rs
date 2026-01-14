77adf32b79cb18e89352c6c968cd5895980ce6c1
Liam Diprose <liam@liamdiprose.com>
2026-01-15 12:31:19 +1300

    Use plural for counter metric

--- src/layers/opentelemetry/mod.rs
+++ src/layers/opentelemetry/mod.rs
@@ -21,7 +21,7 @@ impl<S> Layer<S> for OpenTelemetryMetricsLayer {
     fn layer(&self, service: S) -> Self::Service {
         let meter = global::meter("apalis");
 
-        let task_counter = meter.u64_counter("apalis_task")
+        let task_counter = meter.u64_counter("apalis_tasks")
                                    .build();
 
         let duration_histogram = meter
